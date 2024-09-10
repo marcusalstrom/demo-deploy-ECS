5cc015f801daedcc9066ce7eef8c66f04c3de39a
Noak Jönsson <noakjonsson@gmail.com>
2024-09-10 15:07:58 +0200

    v2 init

--- app/main.py
+++ app/main.py
@@ -3,10 +3,15 @@ from itertools import cycle
 
 app = Flask(__name__)
 
-
+messages = cycle([
+    "DevOps is where its at yo!",
+    "I eat bugs for breakfast",
+    "I much prefer this to my old job",
+    "Spoon is NOT the best Java source code analyser in the world!"
+])
 @app.route("/")
 def canary_page():
-    return render_template('canary.html', msg = "Spoon is the best Java source code analyser in the world!")
+    return render_template('canary.html', msg = next(messages))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
