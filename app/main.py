d6cea243c8880956600711c39d18b2b3d85dceef
Noak Jönsson <noakjonsson@gmail.com>
2024-09-10 13:29:08 +0200

    typo

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def canary_page(clicked=0):
+def canary_page():
     return render_template('canary.html', msg = "Spoon is the best Java source code analyser in the world!")
 
 if __name__ == "__main__":
