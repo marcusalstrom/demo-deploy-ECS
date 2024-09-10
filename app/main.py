0a4c1425b0f775aa9bd1c8220cca50871f106e4d
Noak Jönsson <noakjonsson@gmail.com>
2024-09-10 16:22:55 +0200

    v2 client side textswap

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from itertools import cycle
 
 app = Flask(__name__)
 
-messages = cycle([
+messages = [
     "DevOps is where its at yo!",
     "I eat bugs for breakfast",
     "I much prefer this to my old job",
     "Spoon is NOT the best Java source code analyser in the world!"
-])
+]
 @app.route("/")
 def canary_page():
-    return render_template('canary.html', msg = next(messages))
+    return render_template('canary.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
